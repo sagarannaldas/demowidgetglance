5aba524fd96b3301d514f9e6fdf26f861c21a734
sagarannaldas <annal7057@gmail.com>
2025-02-27 19:56:26 +0530

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+t
+Dapp/src/main/java/com/sagarannaldas/demowidgetglance/MainActivity.kt,f/5/f59b75c0ecf730824ec81f21de865d4ed1456766
+@
+build.gradle.kts,d/b/dbcff70658daf80b53ce624f6adcaa529df5ed8d
---
+++ app/src/main/java/com/sagarannaldas/demowidgetglance/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.sagarannaldas.demowidgetglance
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sagarannaldas.demowidgetglance.ui.theme.DemoWidgetGlanceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DemoWidgetGlanceTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DemoWidgetGlanceTheme {
+        Greeting("Android")
+    }
+}
